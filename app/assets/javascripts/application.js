bf27e3529949f53e2edece855cf82a1fc130a6d8
Sean <seanhaines13@gmail.com>
2015-06-07 12:35:12 -0700

    simple commit msg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,6 +51,16 @@
 	    }
 	  });
 
+function initialize() {
+    var mapOptions = {
+      center: { lat: 47.60621, lng: -122.33207},
+      zoom: 12
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+
 });
   }(jQuery));
 
